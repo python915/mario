6fef8e5d084eacfa915ec4a885eaae66e0ffa665
Jack N. <eugesha88@gmail.com>
2023-11-06 15:03:16 +0300

    added enemies

--- Assets/Architecture/_Scripts/Player.cs
+++ Assets/Architecture/_Scripts/Player.cs
@@ -1,62 +1,90 @@
 using UnityEngine;
 
-
-[RequireComponent(typeof(Rigidbody2D))]
-public class Player : MonoBehaviour
+namespace Mario.Assets.Architecture._Scripts
 {
-
-    #region Properties
-    public int HeatPoint => _heatPoint;
-
-    public float RunSpeed => _runSpeed;
-    public float JumpForce => _jumpForce;
-
-    public Rigidbody2D GetRigidbody2D => _rb;
-    #endregion
-
-    #region Field inspector
-    [Header("Player Data")]
-    [SerializeField] private int _heatPoint;
-
-    [Header("Movement Data")]
-    [SerializeField] private float _runSpeed;
-    [SerializeField] private float _jumpForce;
-    #endregion
-
-    #region Player links
-    private Rigidbody2D _rb;
-    #endregion
-
-
-
-
-
-
-
-    // Start is called before the first frame update
-    void Awake()
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class Player : MonoBehaviour
     {
-        _rb = GetComponent<Rigidbody2D>();
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+        #region Properties
+        public int Health => _playerHealth;
+        public int MaxHealth => _maxHealthPlayer;
 
-    class PlayerMovement 
-    {
+        public float SpeedRun => _speedRun;
+        public float ForceJump => _forceJump;
+
+        public Rigidbody2D GetRigidbody2D => _rb;
+        #endregion
+
+        #region Field inspector
+        [Header("Player Data")]
+        [SerializeField] private int _playerHealth;
+        [SerializeField] private int _maxHealthPlayer;
+
+        [Header("Movement Data")]
+        [SerializeField][Range(2, 10)] private float _speedRun;
+        [SerializeField][Range(2, 10)] private float _forceJump;
+        #endregion
+
+        #region
+        [Header("Player links")]
+        private Rigidbody2D _rb;
+        #endregion
+
+
+
+
+
+        // Start is called before the first frame update
+        private void Start()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+        }
+
+        // Update is called once per frame
+        private void Update()
+        {
+            if (Input.GetKeyDown(KeyCode.Space)) Jump();
+        }
+
+        private void FixedUpdate()
+        {
+            Run();
+        }
+
         private void Run()
         {
+            var derectionX = Input.GetAxisRaw("Horizontal");
+            var velocity = new Vector2(derectionX * _speedRun, _rb.velocity.y);
+            _rb.velocity = velocity;
 
         }
 
         private void Jump()
         {
-
+            _rb.AddForce(Vector2.up * _forceJump, ForceMode2D.Impulse);
         }
+
+        private void BigJump()
+        {
+            _rb.AddForce(Vector2.up * (_forceJump * 2), ForceMode2D.Impulse);
+        }
+
+        private void OnTriggerEnter2D(Collider2D collision)
+        {
+            if(collision.gameObject.tag == "Jump")
+            {
+                BigJump();
+                Destroy(collision.gameObject);
+
+            }
+            if(collision.gameObject.tag == "Destroy")
+            {
+                Destroy(collision.gameObject);
+            }
+        }
+
+
     }
 
-    
 }
---
+++ Assets/Architecture/_Scripts/PlayerHealth.cs
@@ -0,0 +1,26 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+namespace Mario.Assets.Architecture._Scripts
+{
+    public class PlayerHealth
+    {
+        private int _bonusHealth;
+        private int _hp = new Player().Health;
+        private int _maxHp = new Player().MaxHealth;
+        
+
+        private void SetHealth()
+        {
+            _hp += _bonusHealth;
+            if (_hp >= _maxHp) _hp = _maxHp;
+        }
+
+        private void TakeDamage(int damage)
+        {
+            _hp -= damage;
+        }
+    }
+
+}
+
